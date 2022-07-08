ca64fb3418ad38d90c71fa6f003ea56c8d290731
악동분홍토끼 <pinkrabbit412@daum.net>
2022-07-09 00:57:39 +0900

    Updated Chapter #2
    1. Updated App.js to satisfy the JavaScript standard (Used Prettier)

--- 02. JSX/jsx_1/src/App.js
+++ 02. JSX/jsx_1/src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 // #1. 상수 선언 부분
-const name = '악동분홍토끼';
+const name = "악동분홍토끼";
 const app_style = {
-	backgroundColor: '#252830',
-	color: '#FCACD3',
-	fontSize: '48px',
-	fontWeight: 'bold',
-	padding: 16 // 단위 미지정시 픽셀(px)로 간주됨
+  backgroundColor: "#252830",
+  color: "#FCACD3",
+  fontSize: "48px",
+  fontWeight: "bold",
+  padding: 16, // 단위 미지정시 픽셀(px)로 간주됨
 };
 
 // #2. 함수 선언 부분
 function App() {
-	return (
-		<>
-		<div className="App" style={app_style}>
-			{name}님, 환영합니다!
-		</div>
-		<div className="reverseStyle">
-		이 페이지는 테스트 페이지입니다.
-		</div>
-		</>
-	);
+  return (
+    <>
+      <div className="App" style={app_style}>
+        {name}님, 환영합니다!
+      </div>
+      <div className="reverseStyle">이 페이지는 테스트 페이지입니다.</div>
+    </>
+  );
 }
 
 export default App;
