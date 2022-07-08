e2628fe62441a2a6617f09fff5854bcbea6cd8fd
악동분홍토끼 <pinkrabbit412@daum.net>
2022-07-09 00:38:42 +0900

    Created Chapter #2

---
+++ 02. JSX/jsx_1/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+// #1. 상수 선언 부분
+const name = '악동분홍토끼';
+const app_style = {
+	backgroundColor: '#252830',
+	color: '#FCACD3',
+	fontSize: '48px',
+	fontWeight: 'bold',
+	padding: 16 // 단위 미지정시 픽셀(px)로 간주됨
+};
+
+// #2. 함수 선언 부분
+function App() {
+	return (
+		<div className="App" style={app_style}>
+			{name}님, 환영합니다!
+		</div>
+	);
+}
+
+export default App;
