5da8a9ef4e0575dba274a846eff7fcf50a6f06d3
악동분홍토끼 <pinkrabbit412@daum.net>
2022-07-19 22:38:15 +0900

    Created Chapter #5

---
+++ 05. Naming ref-DOM/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Validation from './Validation';
+
+function App() {
+  return (
+    <>
+    <Validation />
+    </>
+  );
+}
+
+export default App;
---
+++ 05. Naming ref-DOM/src/Validation.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+class Validation extends Component {
+  state = {
+    access_code: "",
+    clicked: false,
+    validated: false,
+  };
+  whenValueChanged = (event) => {
+    this.setState({ access_code: event.target.value });
+  };
+  whenConnectButtonClicked = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.access_code === "0412",
+    });
+  };
+  render() {
+    return (
+      <>
+        액세스 코드를 입력하십시오.
+        <br />
+        <input
+          type={"password"}
+          value={this.state.access_code}
+          onChange={this.whenValueChanged}
+          className={this.state.clicked ? (this.state.validated ? "success" : "fail") : ""}
+        ></input>
+        &nbsp;
+        <button onClick={this.whenConnectButtonClicked}>접속</button>
+      </>
+    );
+  }
+}
+
+export default Validation;
