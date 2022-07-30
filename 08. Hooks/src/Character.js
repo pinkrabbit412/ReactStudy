17f576824c6abac2494a3476ee412b16a1f2ba66
악동분홍토끼 <pinkrabbit412@daum.net>
2022-07-30 22:48:00 +0900

    Updated Chapter #8

--- 08. Hooks/src/Character.js
+++ 08. Hooks/src/Character.js
@@ -1,19 +1,40 @@
-import { useReducer } from "react";
+import { useReducer, setState } from "react";
 
 function reducer(state, action) {
+  switch (action.name) {
+    case "level, -1":
+      return {
+        ...state,
+        level: state.level - 1
+      }
+    case "level, +1":
+      return {
+        ...state,
+        level: state.level + 1
+      }
+    default:
+      break;
+  }
   return {
     ...state,
-    [action.name]: action.value
+    [action.name]: action.value,
   };
 }
 
 const Character = () => {
-  const [state, dispatch] = useReducer(reducer, { level: 0, nickname: "<닉네임>" });
-  const {level, nickname} = state;
+  const [state, dispatch] = useReducer(reducer, {
+    level: 0,
+    nickname: "<닉네임>",
+  });
+  const { level, nickname } = state;
 
   const whenValueHasChanged = (event) => {
     dispatch(event.target);
   };
+
+  const whenChangeLevelButtonClicked = (event) => {
+    dispatch(event.target);
+  }
 
   return (
     <div className="componentWrapper">
@@ -27,9 +48,19 @@ const Character = () => {
         placeholder="닉네임 입력..."
       />
       &nbsp;
-      <button onClick={}>레벨 -1</button>
+      <button
+        name="level, -1"
+        onClick={whenChangeLevelButtonClicked}
+      >
+        레벨 -1
+      </button>
       &nbsp;
-      <button onClick={}>레벨 +1</button>
+      <button
+        name="level, +1"
+        onClick={whenChangeLevelButtonClicked}
+      >
+        레벨 +1
+      </button>
     </div>
   );
 };
